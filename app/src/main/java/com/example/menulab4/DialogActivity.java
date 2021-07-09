0046088accc9e31e859ef5f5afd107ef520ff8e7
YOONGIN22222222 <leeyi0190@naver.com>
2021-07-09 16:37:50 +0900

    Initial commit

---
+++ app/src/main/java/com/example/menulab4/DialogActivity.java
@@ -0,0 +1,170 @@
+package com.example.menulab4;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class DialogActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button b1,b2,b3,b4,btnCal;
+    int selectMenu=0;
+    String menu[]={"치킨","피자","스파게"};
+    boolean checked[]={true,true,false};
+    //String result=" ";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog);
+        init();
+    }
+    private void init(){
+        b1=findViewById(R.id.button1);
+        b1.setOnClickListener(this);
+        b2=findViewById(R.id.button2);
+        b2.setOnClickListener(this);
+        b3=findViewById(R.id.button3);
+        b3.setOnClickListener(this);
+        b4=findViewById(R.id.button4);
+        b4.setOnClickListener(this);
+        btnCal=findViewById(R.id.btnCal);
+        btnCal.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v){
+        if(v.getId()==R.id.button1){
+            displayDialog();
+        }
+        else if(v.getId()==R.id.button2){
+            displayDialog2();
+        }
+        else if(v.getId()==R.id.button3){
+            displayDialog3();
+        }
+        else if(v.getId()==R.id.button4){
+            displayDialog4();
+        }
+        else if(v.getId()==R.id.btnCal){
+            showDatePicker(v);
+        }
+    }
+
+    private void displayDialog(){
+        Toast.makeText(this,"Didalog",Toast.LENGTH_LONG).show();
+        AlertDialog.Builder dlg=new AlertDialog.Builder(this);
+        dlg.setTitle("기본 대화상자1");
+        dlg.setMessage("대화상자 메세");
+        dlg.setIcon(R.mipmap.ic_launcher);
+        dlg.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                displayToast(null);
+            }
+        });
+        dlg.show();
+    }
+
+    //라디오 버튼
+    private void displayDialog2(){
+        Toast.makeText(this,"Didalog",Toast.LENGTH_LONG).show();
+        AlertDialog.Builder dlg=new AlertDialog.Builder(this);
+        dlg.setTitle("라디오 버튼 대화상");
+        dlg.setSingleChoiceItems(menu, selectMenu, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) { //which= 라디오에서 몇번이 선택된지
+                selectMenu=which;
+            }
+        });
+        dlg.setIcon(R.mipmap.ic_launcher);
+        dlg.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                displayToast(menu[selectMenu]+"가 선택되었습니다.");
+            }
+        });
+        dlg.show();
+    }
+
+    private void displayDialog3(){
+        Toast.makeText(this,"Didalog",Toast.LENGTH_LONG).show();
+        AlertDialog.Builder dlg=new AlertDialog.Builder(this);
+        dlg.setTitle("체크박스 대화상자");
+        dlg.setMultiChoiceItems(menu, checked, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked){ //라디오랑 다른 ischecked
+                checked[which]=isChecked;
+            }
+        }); //두번째 boolean배열
+
+        dlg.setIcon(R.mipmap.ic_launcher);
+        dlg.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String result="";
+                for(int i=0;i<3;i++){
+                    if(checked[i]==true){ result=result+menu[i]; } }
+
+                if(result=="") displayToast("아무것도 선택되지 않았습니다");
+                else displayToast(result+"가 선택되었습니다.");
+            }
+        });
+        dlg.show();
+    }
+
+
+    private void displayDialog4(){
+
+
+        //inflate
+        View view=View.inflate(this,R.layout.dialog,null);
+        EditText editText=view.findViewById(R.id.etMsg);
+
+
+        Toast.makeText(this,"Didalog",Toast.LENGTH_LONG).show();
+        AlertDialog.Builder dlg=new AlertDialog.Builder(this);
+        dlg.setTitle("사용자 정의 대화상자");
+
+        dlg.setIcon(R.mipmap.ic_launcher);
+        dlg.setView(view);
+
+        dlg.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                displayToast("입력한 메모"+editText.getText().toString());
+
+            }
+        });
+        dlg.setPositiveButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                displayToast("취소되었습니다");
+            }
+        });
+        dlg.show();
+    }
+
+    public void showDatePicker(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(),"datePicker");
+    }
+
+
+
+    public void displayToast(String msg){
+        if(msg==null)msg="Ok Clicked";
+
+        Toast.makeText(this,msg,Toast.LENGTH_LONG).show();
+    }
+
+}
